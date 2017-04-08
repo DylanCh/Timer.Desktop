df4838cb6aade42f44fb5c562d42df7006814fa9
Dylan Chen <hchen004@citymail.cuny.edu>
2017-04-07 23:33:11 -0400

    Create Main.java

---
+++ src/desktopTimer/Main.java
@@ -0,0 +1,34 @@
+package desktopTimer;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.*;
+import javafx.stage.*;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        //FXMLLoader fxmlLoader = new FXMLLoader();
+        Parent root = FXMLLoader.load(getClass().getResource("desktopTimer.fxml"));
+        //Controller controller = (Controller) fxmlLoader.getController();
+
+        primaryStage.setTitle("Timer");
+
+        primaryStage.setOnCloseRequest(event -> Platform.exit());
+
+        Scene scene = new Scene(root,300,275);
+        scene.getStylesheets().add("/desktopTimer/Style.css");
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+}
