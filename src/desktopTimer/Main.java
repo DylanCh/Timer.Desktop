b1a842feea2b48dbcca305b0dc5a6b1de354667e
Dylan Chen <hchen004@citymail.cuny.edu>
2017-04-07 23:53:07 -0400

    Add files via upload

--- src/desktopTimer/Main.java
+++ src/desktopTimer/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         //FXMLLoader fxmlLoader = new FXMLLoader();
-        Parent root = FXMLLoader.load(getClass().getResource("desktopTimer.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("desktopTimer.xml"));
         //Controller controller = (Controller) fxmlLoader.getController();
 
         primaryStage.setTitle("Timer");
